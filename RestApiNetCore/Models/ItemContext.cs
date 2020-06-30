8f7aefc3b0739ad16571c782c70a221dbda60332
pduda98 <pduda.98@gmail.com>
2020-06-30 18:45:44 +0200

    first commit

---
+++ RestApiNetCore/Models/ItemContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RestApiNetCore.Models
+{
+    public class ItemContext : DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=db/testdb.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().ToTable("items");
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
